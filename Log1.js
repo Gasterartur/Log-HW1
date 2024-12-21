97ab2ea12bef5d7f80ed84ccb2c624125059e913
Artur Gaster <gasterartur@googlemail.com>
2024-12-21 13:24:33 +0100

    First

---
+++ Log1.js
@@ -0,0 +1,12 @@
+
+let num1 = parseInt(prompt("Введите первое число:"));
+let num2 = parseInt(prompt("Введите второе число:"));
+let num3 = parseInt(prompt("Введите третье число:"));
+
+
+let sum = num1 + num2 + num3;
+
+
+console.log("Сумма чисел:", sum);
+
+document.getElementById("input").value = `Сумма чисел: ${sum}`;
